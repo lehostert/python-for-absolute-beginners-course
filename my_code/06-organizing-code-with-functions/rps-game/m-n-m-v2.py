64ae55af89f8d2b4a9aaf388b3e74483beb9d2b2
Lauren E. Hostert <laurenehostert@gmail.com>
2021-08-14 11:16:00 -0500

    Add Chapter 6 M-n-M game updates

--- my_code/06-organizing-code-with-functions/rps-game/m-n-m-v2.py
+++ my_code/06-organizing-code-with-functions/rps-game/m-n-m-v2.py
@@ -18,21 +18,19 @@ def play_game():
     mm_count = random.randint(1, 100)
     attempt_limit = 5
     attempts = 0
+
     while attempts < attempt_limit:
         guess = int(input("How many M&Ms are in the jar? ")) #increment attempts +1
         attempts += 1
-    if mm_count == guess:
-        print(f"You've won! The number was {mm_count}. Lunch is on us!")
-        # break
-    elif guess < mm_count:
-        print("Sorry, that's too LOW.")
-    else:
-        print("That's too HIGH.")
+        if mm_count == guess:
+            print(f"You've won! The number was {mm_count}. Lunch is on us!")
+            # break
+        elif guess < mm_count:
+            print("Sorry, that's too LOW.")
+        else:
+            print("That's too HIGH.")
 
-    print(f"I can't believe you got it in {attempts} attempts")
-
-
-def get_guess():
+    print(f"You finished in {attempts} attempts")
 
 
 if __name__ == "__main__":
