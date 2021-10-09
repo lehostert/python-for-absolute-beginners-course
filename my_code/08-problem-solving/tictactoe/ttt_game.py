d5c38274bdc2dc03475eb7719ca85216d7be6f54
Lauren E. Hostert <laurenehostert@gmail.com>
2021-10-09 14:02:56 -0500

    Working TTT Game

--- my_code/08-problem-solving/tictactoe/ttt_game.py
+++ my_code/08-problem-solving/tictactoe/ttt_game.py
@@ -55,6 +55,7 @@ def main():
     print(f"Game over {player} has won!")
     show_board(board)
 
+
 def choose_location(board, symbol):
     row = int(input("Choose which row:"))
     column = int(input("Choose which column:"))
@@ -73,6 +74,7 @@ def choose_location(board, symbol):
     board[row][column] = symbol
     return True
 
+
 def show_board(board):
     for row in board:
         print("| ", end='')
@@ -81,47 +83,47 @@ def show_board(board):
             print(symbol, end=" | ")
         print()
 
+
 def announce_turn(player):
     print("")
     print(f"It's {player}'s turn. Here is the board:")
     print("")
 
+
 def find_winner(board):
-    # TODO: implement how we check for a winner
-    # Win by rows
-    rows = board
-    for row in rows:
-        symbol1 = row[0]
-        if symbol1 and all(symbol1 == cell for cell in row):  # if this does not happen it will continue to WBC
+    sequences = get_winning_sequences(board)
+    for cells in sequences:
+        symbol1 = cells[0]
+        if symbol1 and all(symbol1 == cell for cell in cells):  # If this does not happen continue & Return FALSE
             return True
 
-    # Win by columns WBC
-    columns = []
-    for col_index in range(0,3):  # start in inclusive while stop is exclusive in range function
-        col= [
+    return False
+
+
+def get_winning_sequences(board):
+    sequences = []
+
+    # Win by rows
+    rows = board
+    sequences.extend(rows)
+
+    # Win by columns
+    for col_index in range(0, 3):  # start in inclusive while stop is exclusive in range function
+        col = [
             board[0][col_index],
             board[1][col_index],
             board[2][col_index],
         ]
-        columns.append(col)
+        sequences.append(col)
 
-    for col in columns:
-        symbol1 = col[0]
-        if symbol1 and all(symbol1 == cell for cell in col):  # if this does not happen it will continue to WBD
-            return True
-
-
-    # Win by diagonals WBD
+    #Win by diagonals
     diagonals = [
         [board[0][0],board[1][1],board[2][2]],
         [board[0][2],board[1][1],board[2][0]]
     ]
-    for diag in diagonals:
-        symbol1 = diag[0]
-        if symbol1 and all(symbol1 == cell for cell in diag):  # if this does not happen it will continue to WBD
-            return True
+    sequences.extend(diagonals)
+    return sequences
 
-    return False
 
 if __name__ == '__main__':
     main()
